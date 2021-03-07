27a883ec3341aecd4939c814610eff3e1420b5c6
Damián Silvani <munshkr@gmail.com>
2021-03-07 14:56:09 -0300

    Define Socket send(), close() and on() methods

--- src/Socket.ts
+++ src/Socket.ts
@@ -3,14 +3,35 @@ import Client from "./Client";
 class Socket {
   protected _client: Client;
   protected _port: number;
+  protected _open: boolean;
 
   contructor(client: Client, port: number) {
     this._client = client;
     this._port = port;
+    this._open = true;
   }
 
   get port(): number {
     return this._port;
+  }
+
+  send(message: any): boolean {
+    if (!this._open) return false;
+    this._client.send(this.port, message);
+    return true;
+  }
+
+  on(cb: (message: any) => void): boolean {
+    if (!this._open) return false;
+    // TODO ...
+    return true;
+  }
+
+  close(): boolean {
+    if (!this._open) return false;
+    this._open = false;
+    this._client.removeSocket(this._port);
+    return true;
   }
 }
 
