9d9e63632470090a5bb686479de0d3a8d75102e9
Damián Silvani <munshkr@gmail.com>
2021-03-07 09:44:31 -0300

    Create basic test placeholder

---
+++ src/Client.test.ts
@@ -0,0 +1,8 @@
+import Client from './Client';
+// import WebSocket from 'isomorphic-ws';
+
+test('constructor', () => {
+  const client = new Client();
+  expect(client.url).toBe("ws://localhost:8000");
+  // const ws = new WebSocket('ws://localhost:8000');
+});
