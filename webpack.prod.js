0c4e618c1444bf009c4a1252e0d817e880b71ecb
Damián Silvani <munshkr@gmail.com>
2021-03-10 20:56:29 -0300

    Add bundle analyzer plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require('webpack-merge');
 const [commonNodeConfig, commonBrowserConfig] = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const productionConfig = {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new BundleAnalyzerPlugin(),
+  ],
 }
 
 const nodeConfig = merge(commonNodeConfig, productionConfig);
