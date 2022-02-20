83191bc92b0a05df337b64c5c5e40de78daa93c1
Damián Silvani <munshkr@gmail.com>
2022-02-20 10:49:07 -0300

    feat: Export `Message` and `Bundle` classes from osc-js

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import Client from './Client';
-import Channel from './Channel';
-import Server from './Server';
-import * as defaults from './defaults';
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import Client from "./Client";
+import Channel from "./Channel";
+import Server from "./Server";
+import * as defaults from "./defaults";
+import { Message, Bundle } from "osc-js";
 
 const version = __VERSION__;
 
-export { Client, Channel, Server, version, defaults };
+const OSC = {
+  Message,
+  Bundle,
+};
+
+export { Client, Channel, Server, OSC, version, defaults };
